d2f3311cbfd247fd451f65bfd5e8d24de6f25d8c
repo-chazel <62423408+repo-chazel@users.noreply.github.com>
2020-04-30 17:36:09 +0900

    Add files via upload
    about 'this' keyword

---
+++ this_keyword.js
@@ -0,0 +1,9 @@
+Global Reference : window, global object
+
+Function Invocation : window, global object
+
+.call or .apply invocation : the first argument to .call or .apply
+
+constructor : a new object created for that invocation
+
+Method Invocation : object on the left of the call time dot
