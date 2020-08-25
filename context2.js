b70b7d8b9c50dd57f4eba342986fcb715d228feb
engineve <yeindap@gmail.com>
2020-08-25 22:56:38 +0900

    Update context2.js
    context concept added

--- context2.js
+++ context2.js
@@ -8,4 +8,46 @@ const vehicle = {
     }
   }
 
-  vehicle.drive()
+  const vehicle1 = {
+    car: 'Benz',
+    drive: function() {
+      console.log('1',this) // 1 { car: 'Benz', drive: [Function: drive] }
+      var inFunc = () => {
+        console.log('2',this)// 2 { car: 'Benz', drive: [Function: drive] }
+      }
+      inFunc()
+    }
+  }
+
+  const vehicle2 = {
+    car: 'Hyundai',
+    drive: function() {
+      console.log('1',this) // 1 { car: 'Benz', drive: [Function: drive] }
+      var inFunc = () => {
+        console.log('2',this)// 2 { car: 'Benz', drive: [Function: drive] }
+      }
+      return inFunc.bind(this) // Object vehicle2
+    }
+  }
+
+  const vehicle3 = {
+    car: 'Kia',
+    drive: function() {
+      console.log('1',this) 
+      let self=this;
+      var inFunc = () => {
+        console.log('2',self)
+      }
+      return inFunc
+    }
+  }
+
+  vehicle.drive() //  1 { car: 'BMW', drive: [Function: drive] }
+
+  vehicle1.drive() // 1 { car: 'Benz', drive: [Function: drive] }, 2 { car: 'Benz', drive: [Function: drive] }
+
+  vehicle2.drive() // 1 { car: 'Hyundai', drive: [Function: drive] }
+
+  vehicle2.drive()() // 1 { car: 'Hyundai', drive: [Function: drive] }, 2 { car: 'Hyundai', drive: [Function: drive] }
+
+  vehicle3.drive()() // 1 { car: 'Kia', drive: [Function: drive] }, 2 { car: 'Kia', drive: [Function: drive] } 
