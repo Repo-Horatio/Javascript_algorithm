209fc8047f15735fa344b2055d9d2bfceb0f694f
engineve <yeindap@gmail.com>
2020-08-26 04:14:42 +0900

    Create extendsNoodle.js
    extends class practice

---
+++ extendsNoodle.js
@@ -0,0 +1,28 @@
+class Noodle{
+    constructor(name, price){
+        console.log(this);
+        this.name=name;
+        this.price=price;
+    }
+    info(){
+        console.log(`${this.name} is ${this.price} won`);
+    }
+}
+
+class Product extends Noodle{
+    constructor(name, price){
+        super(name, price);
+    }
+    intro(){
+        console.log(`${this.name} * 1 == ${this.price}`);
+    }
+}
+
+const seafoodNoodle=new Product("seafood", '1500'); // Product {}
+const soybeanNoodle=new Product('soybean','1200'); // Product {}
+
+seafoodNoodle.info() // seafood is 1500 won
+seafoodNoodle.intro() // seafood * 1 == 1500
+
+soybeanNoodle.info() // soybean is 1200 won
+soybeanNoodle.intro() // soybean * 1 == 1200
