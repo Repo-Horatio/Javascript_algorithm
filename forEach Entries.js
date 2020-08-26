c8bddc27d1718d78373010c027e056904534b097
engineve <yeindap@gmail.com>
2020-08-27 04:26:35 +0900

    Create forEach Entries.js
    Object.keys, Object.values, Object.entries

---
+++ forEach Entries.js
@@ -0,0 +1,25 @@
+let obj={car1: 'BENZ', car2: "BMW", car3:"Hyundai"};
+
+Object.keys(obj).forEach((key, idx)=>console.log(key, obj[key]))
+
+// car1 BENZ
+// car2 BMW
+// car3 Hyundai
+
+Object.values(obj).forEach(car=> console.log(car))
+
+// BENZ
+// BMW
+// Hyundai
+
+Object.entries(obj).forEach(car=>console.log(car))
+
+// [ 'car1', 'BENZ' ]
+// [ 'car2', 'BMW' ]
+// [ 'car3', 'Hyundai' ]
+
+Object.entries(obj).map(car=> console.log(`${car[1]} is ${car[0]}`))
+
+// BENZ is car1
+// BMW is car2
+// Hyundai is car3
