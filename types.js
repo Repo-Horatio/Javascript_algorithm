16e4a5835451d20997d2be8e042fd7dc551e5353
engineve <yeindap@gmail.com>
2020-09-01 20:14:44 +0900

    Create types.js
    javascript types

---
+++ types.js
@@ -0,0 +1,10 @@
+console.log(typeof Infinity); // number
+console.log(typeof NaN); // number
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
+console.log(typeof globalThis); // object
+
+// string, boolean, number are primitive, String, Number, Boolean are object references
+console.log(true.toString()); // true
+console.log(Boolean(true).toString()); // true
+console.log(String('true').toString()); // true
