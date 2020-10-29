812b8a0d0f511411b9988a31a45ab306b031cc11
engineve <yeindap@gmail.com>
2020-10-30 00:01:05 +0900

    Update pbkdf2pwd.js
    try catch statement added

--- pbkdf2pwd.js
+++ pbkdf2pwd.js
@@ -1,13 +1,18 @@
 const bkfd2Password=require('pbkdf2-password');
 const hasher=bkfd2Password();
-const assert=require('assert');
+const assert=require('assert').strict;
 
 var options={password:'hi'};
 
 hasher(options,  function(err, pass, salt, hash){
     options.salt=salt;
     hasher(options, function(err, pass, salt, hash2){
-        assert.deepStrictEqual(hash2, hash);
-        console.log(pass, salt, hash, hash2)
+        try{
+            assert.deepStrictEqual(hash2, hash);
+            console.log('OK');
+            console.log(pass, salt, hash, hash2);
+        }catch(error){
+            console.log("Error : ", error);
+        }
     })
 });
