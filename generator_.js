0ccad89e8b10fcd5a3a5920902187b2907f0d330
engineve <yeindap@gmail.com>
2020-11-14 15:03:35 +0900

    Update generator_.js
    generator concept

--- generator_.js
+++ generator_.js
@@ -6,3 +6,17 @@ const gen=generator();
 
 console.log('1', gen); // 1 Object [Generator] {}
 console.log('2', gen.next()); // invoked   2 { value: undefined, done: true }
+
+function* gen1(i){
+    yield i*2;
+    yield i*3;
+}
+
+const g1=gen1(3);
+const g2=g1.next();
+const g3=g1.next();
+const g4=g1.next();
+
+console.log(g2); // { value: 6, done: false }
+console.log(g3); // { value: 9, done: false }
+console.log(g4); // { value: undefined, done: true }
