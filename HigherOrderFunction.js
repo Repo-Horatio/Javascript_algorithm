77d8e333a8a5213d7f00352c8918e8c07a8f3654
engineve <yeindap@gmail.com>
2020-12-12 23:01:08 +0900

    Update HigherOrderFunction.js
    HOF with arrow function

--- HigherOrderFunction.js
+++ HigherOrderFunction.js
@@ -6,5 +6,9 @@ const plusBy=function(N1){
     }
 }
 
+const plusBy1=(N1)=>(N2)=>N1+N2
+
 const plusByTen=plusBy(10);
+const plusByTen1=plusBy1(10);
 console.log(plusByTen(5)); // 15
+console.log(plusByTen1(5)); // 15
