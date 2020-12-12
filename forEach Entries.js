9ab0fa2d37ba36dd81c8600dfa78129f17a2c01f
engineve <yeindap@gmail.com>
2020-12-12 17:51:27 +0900

    Update forEach Entries.js
    let of with Object.entries

--- forEach Entries.js
+++ forEach Entries.js
@@ -23,3 +23,11 @@ Object.entries(obj).map(car=> console.log(`${car[1]} is ${car[0]}`))
 // BENZ is car1
 // BMW is car2
 // Hyundai is car3
+
+for (let [key, value] of Object.entries(obj)){
+    console.log(`${key}: ${value}`);
+}
+
+// car1: BENZ
+// car2: BMW
+// car3: Hyundai
