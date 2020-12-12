c76775914fdec06aaa0f6529a9b6e312c7139b71
engineve <yeindap@gmail.com>
2020-12-12 22:37:12 +0900

    Update types.js
    Object.is()   -0, +0

--- types.js
+++ types.js
@@ -8,3 +8,6 @@ console.log(typeof globalThis); // object
 console.log(true.toString()); // true
 console.log(Boolean(true).toString()); // true
 console.log(String('true').toString()); // true
+
+console.log(-0===+0); // true
+console.log(Object.is(-0,+0)); // false
