7507a3c7eb2b7fe5660dc39f53edc9aee67af708
enginepdf <yeindap@gmail.com>
2020-12-20 13:29:50 +0900

    Update IterativeFibonacci.js
    added lett

--- IterativeFibonacci.js
+++ IterativeFibonacci.js
@@ -2,10 +2,10 @@ var nthFibonacci = function(n) {
   if(n===1 || n===2){
     return 1;
   }
-  twobefore=1;
-  onebefore=1;
-  next=0;
-  for(i=3 ; i<=n ; i++){
+  let twobefore=1;
+  let onebefore=1;
+  let next=0;
+  for(let i=3 ; i<=n ; i++){
     next=twobefore+onebefore;
     twobefore=onebefore;
     onebefore=next;          
