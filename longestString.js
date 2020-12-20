7ef753485c4e185c43e2f2af3a5e9008ba22dca7
enginepdf <yeindap@gmail.com>
2020-12-20 13:23:45 +0900

    Update longestString.js
    added let

--- longestString.js
+++ longestString.js
@@ -2,12 +2,12 @@ function longestString(arr) {
   if(arr.length===0){
     return ''
   }
-  arr1=[]
- for(let i=0 ; i<arr.length ; i++){
-  arr1=arr.filter(function(ar){
-  return arr[i].length<ar.length})
-  if(arr1.length===0){return arr[i]}
-  }
+  let arr1=[]
+    for(let i=0 ; i<arr.length ; i++){
+       arr1=arr.filter(function(ar){
+        return arr[i].length<ar.length})
+    if(arr1.length===0){return arr[i]}
+    }
  
 }
 
