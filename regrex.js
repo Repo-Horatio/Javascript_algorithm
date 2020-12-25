cac46e957fdee3631a79319cbd0b828ab6f9698e
enginepdf <yeindap@gmail.com>
2020-12-25 14:21:02 +0900

    Update regrex.js
    reg exec

--- regrex.js
+++ regrex.js
@@ -7,3 +7,13 @@ let items = str.split(new RegExp('[' + separators.join('') + ']', 'g'));
 console.log(new RegExp('[' + separators.join('') + ']', 'g')); //  /[ +-]/g
 console.log(separators.join('')); //  +-
 console.log(items); // [ '100', '50', '60' ]
+
+
+console.log(/item/.exec("this is an item")); // [ 'item', index: 11, input: 'this is an item', groups: undefined ]
+
+
+let reg = new RegExp('b(a+)e', 'g')
+let arr = reg.exec('abaaaae');
+console.log(reg); //  /b(a+)e/g
+console.log(arr); // [ 'baaaae', 'aaaa', index: 1, input: 'abaaaae', groups: undefined ]
+
