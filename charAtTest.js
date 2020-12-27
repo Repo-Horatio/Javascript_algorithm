ce567bab01c31f585853d733e4440313cbf90728
enginepdf <yeindap@gmail.com>
2020-12-28 02:07:41 +0900

    Create charAtTest.js
    .charAt()

---
+++ charAtTest.js
@@ -0,0 +1,7 @@
+// reference : https://developer.mozilla.org/ko/docs/Web/JavaScript/Reference/Global_Objects/String/charAt
+
+const sentence = 'This is String.charAt() test.';
+
+const index = 5;
+
+console.log(`The character at index ${index} is ${sentence.charAt(index)}`); //  The character at index 5 is i
