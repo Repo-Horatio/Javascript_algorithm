f278112982a9ba6154ada90366ac83cf522d0fef
enginepdf <yeindap@gmail.com>
2020-12-28 01:42:24 +0900

    Create makingDeck.js
    making a Deck

---
+++ makingDeck.js
@@ -0,0 +1,13 @@
+var makingDeck = function() {
+    var suits = [ '♥', '♣', '♠', '♦' ];
+    var values = [ 'A', 2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K' ];
+    var deck = [];
+  
+    suits.forEach(function(suit) {
+      values.forEach(function(value) {
+        deck.push(value + suit);
+      });
+    });
+  
+    return deck;
+  };
