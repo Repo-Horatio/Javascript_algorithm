0db57a3183d3c953a0cf7f2740717af453586ed3
enginepdf <yeindap@gmail.com>
2020-12-28 11:56:50 +0900

    Update arrayWithIndex.js
    add an element with index

--- arrayWithIndex.js
+++ arrayWithIndex.js
@@ -3,3 +3,7 @@ for(let i=0 ; i<['abc'].length ; i++){
         temp[i]=['abc'][i];
 }
 console.log(temp); // [ 'abc' ]
+
+let arr=[];
+arr[5]='an item';
+console.log(arr); // [ <5 empty items>, 'an item' ]
