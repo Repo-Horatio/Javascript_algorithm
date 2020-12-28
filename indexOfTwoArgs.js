20554de170f872e65ff8cc71aee695a22bdf82e1
enginepdf <yeindap@gmail.com>
2020-12-28 21:41:52 +0900

    Create indexOfTwoArgs.js
    indexOf with two arguments

---
+++ indexOfTwoArgs.js
@@ -0,0 +1,4 @@
+// reference : https://developer.mozilla.org/ko/docs/Web/JavaScript/Reference/Global_Objects/String/indexOf
+
+let idx='This is an item'.indexOf('Whale', 15); //  returns -1
+console.log(idx); // -1      str.indexOf(searchValue[, fromIndex])
