6495e8fdcb7397d7f27500d4eec67bfa1553ee31
enginepdf <62423408+enginepdf@users.noreply.github.com>
2021-01-25 14:39:02 +0900

    UPDATE basic.js
    danfo.js methods   fillna, loc etc

--- DanfoJS/basic.js
+++ DanfoJS/basic.js
@@ -1,10 +1,12 @@
 // reference : https://blog.tensorflow.org/2020/08/introducing-danfo-js-pandas-like-library-in-javascript.html
 
 // npm install danfojs-node
+// npm install @tensorflow/tfjs-node
 
 const dfd = require("danfojs-node");
+const tf = require("@tensorflow/tfjs-node")
 
-var s = new dfd.Series([22.5, 24, 27.5]); 
+var s = new dfd.Series([22.5, 24, 27.5]);    // List-like Series
 s.print();
 
 // ╔═══╤══════════════════════╗
@@ -56,7 +58,9 @@ var d1 = new dfd.DataFrame([
                             {'온도':22.5, '판매량 증가율 %':'매출 급증(섭씨 21도부터'},  // row
                             {'온도':24, '판매량 증가율 %':36},
                             {'온도':27.5, '판매량 증가율 %':20},
-                            ]);
+                            ]); 
+                            // '온도': ['22.5, 24, 27.5]
+                            // '판매량 증가율': ['매출 급증(섭씨 21도부터', 36, 20]
 d1.print();
 
 // ╔═══╤═══════════════════╤═══════════════════╗
@@ -67,4 +71,56 @@ d1.print();
 // ║ 1 │ 24                │ 36                ║
 // ╟───┼───────────────────┼───────────────────╢
 // ║ 2 │ 27.5              │ 20                ║
+// ╚═══╧═══════════════════╧═══════════════════╝
+
+
+let num = tf.tensor2d([[1,2,3], [4,5,6]]);  // tf.dtypes.DType   tensor는 일관된 유형을 가진 다차원 배열. 일종의 np.arrays
+let df_num = new dfd.DataFrame(num);
+let tf_tensor = df_num.tensor;
+console.log(tf_tensor);
+// Tensor {
+//     kept: false,
+//     isDisposedInternal: false,
+//     shape: [ 2, 3 ],
+//     dtype: 'float32',
+//     size: 6,
+//     strides: [ 3 ],
+//     dataId: {},
+//     id: 21,
+//     rankType: '2'
+//   }
+
+tf_tensor.print();
+// Tensor
+//     [[1, 2, 3],
+//      [4, 5, 6]]
+
+let fruits = {"Name":["Apples", "Mango", "Banana", undefined],
+            "Count": [NaN, 5, NaN, 10], 
+            "Price": [200, 300, 40, 250]}        
+let df_fruits = new dfd.DataFrame(fruits)
+let df_filled = df_fruits.fillna({columns: ["Name", "Count"], values: ["Apples", df_fruits["Count"].mean()]})
+df_filled.print()
+
+// ╔═══╤═══════════════════╤═══════════════════╤═══════════════════╗
+// ║   │ Name              │ Count             │ Price             ║
+// ╟───┼───────────────────┼───────────────────┼───────────────────╢
+// ║ 0 │ Apples            │ 7.5               │ 200               ║
+// ╟───┼───────────────────┼───────────────────┼───────────────────╢
+// ║ 1 │ Mango             │ 5                 │ 300               ║
+// ╟───┼───────────────────┼───────────────────┼───────────────────╢
+// ║ 2 │ Banana            │ 7.5               │ 40                ║
+// ╟───┼───────────────────┼───────────────────┼───────────────────╢
+// ║ 3 │ Apples            │ 10                │ 250               ║
+// ╚═══╧═══════════════════╧═══════════════════╧═══════════════════╝
+
+let sub_df = df_filled.loc({ rows: ["0:2"], columns: ["Name", "Price"] })
+sub_df.print()
+
+// ╔═══╤═══════════════════╤═══════════════════╗
+// ║   │ Name              │ Price             ║
+// ╟───┼───────────────────┼───────────────────╢
+// ║ 0 │ Apples            │ 200               ║
+// ╟───┼───────────────────┼───────────────────╢
+// ║ 1 │ Mango             │ 300               ║
 // ╚═══╧═══════════════════╧═══════════════════╝
