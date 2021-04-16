b4c7186c16ecaec742a86cd3da8686135c314b01
enginepdf <yeindap@gmail.com>
2021-04-16 18:25:23 +0900

    Update IIFE.js
    Enumeration in typescript BY IIFE in javascript

--- IIFE.js
+++ IIFE.js
@@ -7,3 +7,23 @@ var IIFE=(function(){
 
 let result=IIFE.guess()
 console.log(result) // 5
+
+let Menu;
+
+(function (Menu) {
+    Menu[Menu["APPETIZER"]=0] = "APPETIZER";
+    Menu[Menu["MAIN"]=1] = "MAIN";
+    Menu[Menu["DESSERT"]=2] = "DESSERT";
+})(Menu || (Menu = {}));
+
+console.log(Menu[0]); // APPETIZER
+console.log(Menu['DESSERT']); // 2
+console.log(Menu);  // Enumeration in typescript
+// {
+//     '0': 'APPETIZER',
+//     '1': 'MAIN',
+//     '2': 'DESSERT',
+//     APPETIZER: 0,
+//     MAIN: 1,
+//     DESSERT: 2
+// }
