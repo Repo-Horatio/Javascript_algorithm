6b142597eef2d2a1ecc018782d1318638dcb2e64
enginepdf <yeindap@gmail.com>
2021-05-28 00:00:05 +0900

    Update sleep.js
    sleep function with promise and setTimeout()

--- sleep.js
+++ sleep.js
@@ -1,4 +1,15 @@
 function sleep(plus){
     var start = new Date().getTime();
     while (new Date().getTime() < start + plus);
+    console.log('done!')
  }
+
+let sleep1=((time) => {
+     return new Promise((resolve, reject) => {
+         setTimeout(()=>{
+             resolve(console.log('done!')) }, time)
+     })
+ });
+
+ sleep(5000);
+ sleep1(5000);
